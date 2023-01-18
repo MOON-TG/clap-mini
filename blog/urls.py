5b238f4b4953525ee07fbc906a6659a2914c2144
MOON_TG <takeyou@kakao.com>
2023-01-18 17:06:03 +0900

    배터리바 구현 완료

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
    # path('', views.index),
    path('', views.PostList.as_view()),
- 
+   path('createform/', views.createform, name='createform'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Info
 from django.views.generic import ListView
 
@@ -8,6 +8,15 @@ class PostList(ListView):
    model = Info
    ordering = '-pk'
    paginate_by = 1
+
+
+def createform(request):
+   std = Info()
+   std.battery= request.GET['battery']
+   std.color = request.GET['color']
+   std.save()
+
+   return redirect('/')
 
 
 
